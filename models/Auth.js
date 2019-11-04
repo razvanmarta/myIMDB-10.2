5098994a4846420e72e67bc0bb31d2f66ee1aeaf
unknown <Ioana2122>
2019-11-04 20:00:02 +0200

    Added modal

--- models/Auth.js
+++ models/Auth.js
@@ -1,6 +1,13 @@
-var apiURLl = "https://movies-api-siit.herokuapp.com"
-var registerURL = url + "/auth/register";
-var loginURL = url + "/auth/login";
-var logoutURL = url + "/auth/logout";
+// var apiURLl = "https://movies-api-siit.herokuapp.com"
+// var registerURL = url + "/auth/register";
+// var loginURL = url + "/auth/login";
+// var logoutURL = url + "/auth/logout";
+
+var registerButton = document.getElementById('register-button');
+registerButton.addEventListener('click', function(){
+    
+    document.getElementById('modal-auth').showModal();
+    }
+)
 
 function Auth() {}

6b4009c3a63f48993596e97ae0017ab9e7bb4795
Crina Berinde <cberinde@sdl.com>
2019-11-14 22:11:09 +0200

    TODO - improvement

--- views/home.js
+++ views/home.js
@@ -5,6 +5,52 @@ const movieList = document.querySelector(".movieList");
 const apiURL = "https://movies-api-siit.herokuapp.com/movies";
 let newUser = sessionStorage.getItem("userName");
 
+
+// TODO - nu are sens sa fie ID-uri pe fiecare din ele
+// id-urile pot fi puse pe containere
+// de exemplu un id pe nav-bar si celelalte luate dupa text sau dupa clasa sau etc
+/*
+* <ul id="top-navigation" class="navigation-menu">
+            <li class="menu-item home">
+              <i class="fas fa-home fa-lg"></i> Home
+            </li>
+            <li class="menu-item login">
+              <i class="fas fa-user fa-lg"></i> Login
+            </li>
+            <li class="menu-item register">
+              <i class="fas fa-user-plus fa-lg"></i> Register
+            </li>
+            <li class="menu-item logout">
+              <i class="fas fa-sign-out-alt fa-lg"></i> Logout
+            </li>
+          </ul>
+*
+* */
+//const topNavbar = document.getElementById("top-navigation");
+
+
+// TODO - mi-ar placea sa vad variabilele astea globale - macar grupate in obiecte de forma:
+/*
+
+const navbar = {
+    homeBtn: topNavbar.querySelector(".home")
+    ....
+};
+
+// const loginModal = document.getElementById("id02")
+
+const login = {
+    closeLogin: loginModal.getElementById("id02"),
+    ....
+};
+
+
+// asta e pentru a evita "poluarea" scopului global ( obiectului window) cu prea multe variabile
+// punandu-le ca priprietati pe obiecte, ele devin oarecum "ascunse"
+*
+* */
+
+
 // Navbar variables
 const homeBtn = document.getElementById("home-button");
 const loginBtn = document.getElementById("login-button");
